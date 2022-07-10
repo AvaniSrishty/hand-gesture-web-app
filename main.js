7cc5aea560b854cf34b390453ab4c2d42628807b
AvaniSrishty <92068623+AvaniSrishty@users.noreply.github.com>
2022-07-10 19:52:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,3 +22,69 @@ Webcam.set({
     function modelLoaded(){
         console.log('Model Loaded!');
     }
+
+    
+
+    function check() {
+        img = document.getElementById("take_snap");
+        classifier.classify(img, gotResult)
+    }
+    
+    function gotResult(error, result) {
+        if (error) {
+            console.log(error);
+        }
+    
+        else {
+            console.log(result);
+            document.getElementById("result_emotion_name").innerHTML = result[0].label
+            document.getElementById("result_emotion_name2").innerHTML = result[1].label
+            prediction1 = result[0].label
+            prediction2 = result[1].label
+            speech();
+    
+    
+            if (prediction1 == "Best") {
+                document.getElementById("result_emoji").innerHTML = "&#128077"
+            }
+    
+            if (prediction1 == "Amazing") {
+                document.getElementById("result_emoji").innerHTML = "&#128076"
+            }
+    
+            if (prediction1 == "Crossed finger") {
+                document.getElementById("result_emoji").innerHTML = "&#129310"
+            }
+
+            if (prediction1 == "Namaste") {
+                document.getElementById("result_emoji").innerHTML = "&#128591"
+            }
+    
+            if (prediction2 == "Best") {
+                document.getElementById("result_emoji2").innerHTML = "&#128077"
+            }
+    
+            if (prediction2 == "Amazing") {
+                document.getElementById("result_emoji2").innerHTML = "&#128076"
+            }
+    
+            if (prediction2 == "Crossed finger") {
+                document.getElementById("result_emoji2").innerHTML = "&#129310"
+            }
+
+            if (prediction2 == "Namaste") {
+                document.getElementById("result_emoji2").innerHTML = "&#128591"
+            }
+    
+        }
+    
+    
+    }
+    
+    function speech(){
+        var synth = window.speechSynthesis;
+        speech_message = "The first prediction is" +prediction1+ " And the second prediction is" +prediction2;
+        var utterThis = new SpeechSynthesisUtterance(speech_message);
+        synth.speak(utterThis);
+    }
+    
